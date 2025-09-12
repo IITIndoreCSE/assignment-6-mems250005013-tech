b8294d59c9e55a16f0d31a5f80aab32181aa61a9
mems250005013-tech <mems250005013@iiti.ac.in>
2025-09-12 16:13:49 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,16 +1,63 @@
 //  Write a program in C++ with a function called add to add two complex numbers and another function called subtract to subtract one complex number from another. 
+#include <iostream>
+using namespace std;
+
+struct Complex {
+
+    float real;
+    float imag;
+
+    
+    Complex(float r = 0, float i = 0) : real(r), imag(i) {}
+};
 
 
+Complex add(const Complex& c1, const Complex& c2) {
+    Complex result;
+    result.real = c1.real + c2.real;
+    result.imag = c1.imag + c2.imag;
+    return result;
+}
 
 
+Complex subtract(const Complex& c1, const Complex& c2) {
+    Complex result;
+    result.real = c1.real - c2.real;
+    result.imag = c1.imag - c2.imag;
+    return result;
+}
 
 
+void display(const Complex& c) {
+    if (c.imag >= 0)
+        cout << c.real << " + " << c.imag << "i" << endl;
+    else
+        cout << c.real << " - " << -c.imag << "i" << endl;
+}
 
+int main() {
+  double x,y;
+  double a,b;
+  cin>>x>>y;
+  cin>>a>>b;
+    Complex num1(x, y);  
+    Complex num2(a, b);  
 
+    cout << "Complex number 1: ";
+    display(num1);
+    cout << "Complex number 2: ";
+    display(num2);
 
+    Complex sum = add(num1, num2);
+    Complex diff = subtract(num1, num2);
 
+    cout << "Sum of complex numbers: ";
+    display(sum);
+    cout << "Difference of complex numbers: ";
+    display(diff);
 
-
+    return 0;
+}
 
 
 
