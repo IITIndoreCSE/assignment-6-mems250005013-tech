4c70efd57a8c0e672fb4da6b0d684336eeb2a2de
mems250005013-tech <mems250005013@iiti.ac.in>
2025-11-10 19:10:14 +0530

    Update Q5.cpp

--- Q5.cpp
+++ Q5.cpp
@@ -1,40 +1,36 @@
 // Write a program to populate a character array with letters of the alphabet at random; subsequently the letters in the array should be arranged alphabetically (without using the sort function).
+
 #include <iostream>
-#include <vector>
-#include <algorithm> 
 using namespace std;
+int main(){
+int n;
+cout << "Enter size of array:\n";
+cin >> n ;
+char arr[n ];
 
-int main() {
-    int n;
-    cout << "Enter number of letters: ";
-    cin >> n;
-
-    vector<char> letters(n);
-    cout << "Enter " << n << " letters:\n";
-    for (int i = 0; i < n; i++) {
-        cin >> letters[i];
-    }
-
-    sort(letters.begin(), letters.end());
-
-    
-    cout << "{";
-    for (int i = 0; i < n; i++) {
-        cout << "'" << letters[i] << "'";
-        if (i < n - 1) cout << ", "; 
-    }
-    cout << "}" << endl;
-
-    return 0;
+cout << "Enter elements of array:\n";
+for (int i=0; i<n; i++){
+    cin >> arr[i];
 }
 
+// sorting without sort function
+for (int i=0; i<n-1; i++){
+    for (int j=0; j<n-i-1; j++){
+        if (arr[j] > arr[j+1]){
+           char temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+        }
+      }
+    }
 
-
-
-
-
-
-
+cout << "Sorted array is:\n[";
+for (int i=0; i<n; i++){
+    cout << arr[i] << " ";
+}
+    cout<< "]";
+return 0;
+}
 
 
 
